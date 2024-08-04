dropout fixed. things are looking pretty good, but having trouble with scaling.

scripts/train.py
@@ -30,13 +30,13 @@ DEVICE = f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu"
 model_args = dict(
     blocks=4,
     width=22,
-    pe_dim=16,
+    pe_dim=22,
 )
 
 config = dict(
     **model_args,
     batch_size=128,
-    lr=1e-3,
+    lr=[(0, 1e-3)],
 )
 
 EPOCHS = 1000
@@ -52,7 +52,7 @@ loss_function = nn.CrossEntropyLoss()
 model = Spectracles(num_classes=10, input_channels=3, **model_args)
 model = model.to(DEVICE)
 
-optimizer = AdamW(model.parameters(), lr=config["lr"], weight_decay=0.01)
+optimizer = AdamW(model.parameters(), lr=config["lr"][0][1], weight_decay=0.01)
 # scheduler = StepLR(optimizer, step_size=1, gamma=0.01)
 
 if args.print_params:
@@ -104,6 +104,12 @@ if not args.print_params:
     train_accuracy = 0
     test_accuracy = 0
     for epoch in range(EPOCHS):
+
+        for e, lr in config["lr"]:
+            if epoch == e:
+                for param_group in optimizer.param_groups:
+                    param_group["lr"] = lr
+
         model.train()
         pbar = tqdm(train_loader, leave=False)
 
@@ -136,9 +142,6 @@ if not args.print_params:
             )
 
         train_accuracy = correct / total
-
-        # if epoch == 20:
-        #     scheduler.step()
 
         model.eval()
         if SAVE:

src/layers.py
@@ -45,10 +45,17 @@ class CompAct(nn.Module):
         x: Tensor,
     ) -> Tensor:
         return torch.complex(self.activation(x.real), self.activation(x.imag))
+    
 
+class PhaseReLU(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        scales = torch.cos(
+            (torch.pi / 4) - torch.angle(x)
+        )
+        # scales = (scales + 1) / 2 # [0, 1]
+        x = x * scales.abs()
+        return x
 
-def recenter(x: Tensor, dim: tuple) -> Tensor:
-    return x - torch.mean(x, dim=dim, keepdim=True)
 
 
 class MagExpLin(nn.Module):
@@ -103,11 +110,25 @@ def get_rotary_position_vectors(shape, num_frequencies, device):
 
     return positions
 
+class GaussianDropout(nn.Module):
+    def __init__(self, p: float = 0.5):
+        super().__init__()
+        self.p = p
+
+    def forward(self, x: Tensor) -> Tensor:
+        if self.training:
+            # Taken from https://arxiv.org/pdf/1506.02557, "Variational Dropout and the Local Reparameterization Trick" by Kingma et al., 2015, section 3, paragraph 1.
+            mask = torch.randn(size=x.shape, device=x.device) * (self.p / (1 - self.p)) + 1
+            x = x * mask
+            x = x / (1 - self.p)
+        return x
+
 
 class ComplexMLP(nn.Module):
     def __init__(
         self,
         widths: list[int],
+        dropout = False,
     ):
 
         super().__init__()
@@ -115,11 +136,18 @@ class ComplexMLP(nn.Module):
         self.layers = nn.Sequential()
 
         for i, (dim_in, dim_out) in enumerate((zip(widths[:-1], widths[1:]))):
+            
+            if dropout:
+                self.layers.append(GaussianDropout(0.1))
+
             self.layers.append(ComplexLinear(dim_in, dim_out))
 
             if i != len(widths) - 2:
                 self.layers.append(CompExp(dim_out))
                 self.layers.append(CompAct(nn.LeakyReLU(0.1)))
+                self.layers.append(CompExp(dim_out))
+                self.layers.append(CompAct(nn.LeakyReLU(0.1)))
+                self.layers.append(CompExp(dim_out))
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.layers(x)

src/model.py
@@ -30,26 +30,25 @@ class Spectracles(nn.Module):
         self.magnorm_in = MagExpLin(width, power=1.0)
 
         # length of this list is one longer than the number of layers the MLP will actually have
-        mlp_widths = [width] * 3
+        mlp_widths = [width, width, width]
+        pe_mlp_widths = [pe_dim, width, width]
 
-        self.pe_mlp = ComplexMLP([pe_dim] * 2 + [width])
-
-        self.post_fft_mlps = nn.ModuleList()
-        self.freq_magnorms = nn.ModuleList()
+        self.freq_pe_mlps = nn.ModuleList()
+        self.post_fft_magnorms = nn.ModuleList()
         self.freq_mlps = nn.ModuleList()
 
-        self.post_ifft_mlps = nn.ModuleList()
-        self.pixel_magnorms = nn.ModuleList()
+        self.pixel_pe_mlps = nn.ModuleList()
+        self.post_ifft_magnorms = nn.ModuleList()
         self.pixel_mlps = nn.ModuleList()
 
         for _ in range(blocks):
-            self.post_fft_mlps.append(ComplexMLP(mlp_widths))
-            self.freq_magnorms.append(MagExpLin(width, power=0.5))
-            self.freq_mlps.append(ComplexMLP(mlp_widths))
+            self.freq_pe_mlps.append(ComplexMLP(pe_mlp_widths))
+            self.post_fft_magnorms.append(MagExpLin(width, power=0.5))
+            self.freq_mlps.append(ComplexMLP(mlp_widths, dropout=True))
 
-            self.post_ifft_mlps.append(ComplexMLP(mlp_widths))
-            self.pixel_magnorms.append(MagExpLin(width, power=0.5))
-            self.pixel_mlps.append(ComplexMLP(mlp_widths))
+            self.pixel_pe_mlps.append(ComplexMLP(pe_mlp_widths))
+            self.post_ifft_magnorms.append(MagExpLin(width, power=0.5))
+            self.pixel_mlps.append(ComplexMLP(mlp_widths, dropout=True))
 
         self.out_magnorm = MagExpLin(width, power=1.0)
         self.out_proj = ComplexLinear(width, num_classes, bias=True)
@@ -77,27 +76,29 @@ class Spectracles(nn.Module):
                 device=x.device,
             ).unsqueeze(0)
 
-        self.processed_pos_enc = self.pe_mlp(self.pos_enc)
-
         # Bounce back and forth between pixel and frequency spaces
         for i in range(self.num_layers):
 
             residual = x
 
+            
             x = torch.fft.fftn(x, dim=(1, 2), norm="ortho")
-            x = self.post_fft_mlps[i](x)
 
             # x = torch.cat([x, self.pos_enc.expand(b, -1, -1, -1)], dim=-1)
-            x = x * self.processed_pos_enc
-            x = self.freq_magnorms[i](x)
+            freq_implicit_filters = self.freq_pe_mlps[i](self.pos_enc)
+            x = x * freq_implicit_filters
+
+            x = self.post_fft_magnorms[i](x)
             x = self.freq_mlps[i](x)
 
+            
             x = torch.fft.ifftn(x, dim=(1, 2), norm="ortho")
-            x = self.post_ifft_mlps[i](x)
 
             # x = torch.cat([x, self.pos_enc.expand(b, -1, -1, -1)], dim=-1)
-            x = x * self.processed_pos_enc
-            x = self.pixel_magnorms[i](x)
+            pixel_implicit_filters = self.pixel_pe_mlps[i](self.pos_enc)
+            x = x * pixel_implicit_filters
+            x = self.post_ifft_magnorms[i](x)
+            
             x = self.pixel_mlps[i](x)
 
             x = x + residual