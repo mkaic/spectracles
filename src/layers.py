d19675ecf236110d0f97664b792538d09d076d4d
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-08-06 00:12:43 +0000

    all changes didn't push for some reason?

--- src/layers.py
+++ src/layers.py
@@ -119,11 +119,11 @@ class LinearCombination(nn.Module):
     def __init__(self, width):
         super().__init__()
         self.width = width
-        self.weights = nn.Parameter(torch.zeros((width,)))
+        self.weights_a = nn.Parameter(torch.ones((width,)))
+        self.weights_b = nn.Parameter(torch.ones((width,)))
 
     def forward(self, a: Tensor, b: Tensor) -> Tensor:
-        weights = torch.sigmoid(self.weights)
-        return a * weights + b * (1 - weights)
+        return a * self.weights_a + b * self.weights_b
 
 
 class ComplexMLP(nn.Module):
